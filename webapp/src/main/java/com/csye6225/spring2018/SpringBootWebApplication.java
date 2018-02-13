1364d34399e163c3f0b1dc9d785ec923a23c2166
danish9412 <danish9412@gmail.com>
2018-02-12 21:25:29 -0500

    file upload to directory

--- webapp/src/main/java/com/csye6225/spring2018/SpringBootWebApplication.java
+++ webapp/src/main/java/com/csye6225/spring2018/SpringBootWebApplication.java
@@ -1,7 +1,12 @@
 package com.csye6225.spring2018;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootWebApplication {
@@ -10,4 +15,21 @@ public class SpringBootWebApplication {
     SpringApplication.run(SpringBootWebApplication.class, args);
   }
 
+  //Tomcat large file upload connection reset
+  @Bean
+  public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+
+    TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+
+    tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+      if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+        //-1 means unlimited
+        ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+      }
+    });
+
+    return tomcat;
+
+  }
 }
+
