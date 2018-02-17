03acc6e4542f2c958371be180151760bd5d123d2
danish9412 <danish9412@gmail.com>
2018-02-16 22:19:06 -0500

    updating process

---
+++ webapp/src/main/java/com/csye6225/spring2018/configuration/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.csye6225.spring2018.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/home").setViewName("index");
+//        registry.addViewController("/").setViewName("welcome");
+//        registry.addViewController("/hello").setViewName("hello");
+//        registry.addViewController("/login").setViewName("Form");
+//    }
+
+}
